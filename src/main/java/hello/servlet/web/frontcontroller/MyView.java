36ecb229c38094f30597133604365613e30c4841
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-11 13:28:15 +0900

    MVC 프레임워크 만들기 V3: Model 추가

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @AllArgsConstructor
 public class MyView {
@@ -18,4 +19,13 @@ public class MyView {
         dispatcher.forward(request, response);
     }
 
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach(request::setAttribute);
+    }
 }
